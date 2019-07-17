cf2fdcb8944e5c11dcec025538d975cc9e7fa80f
Christian Boulanger <info@bibliograph.org>
2019-07-17 08:07:29 +0200

    Merging changes from master

--- compile.js
+++ compile.js
@@ -1,3 +1,5 @@
+const {execSync} = require('child_process');
+const path = require("path");
 qx.Class.define("qxl.packagebrowser.compile.LibraryApi", {
   extend: qx.tool.cli.api.LibraryApi,
 
@@ -6,34 +8,33 @@ qx.Class.define("qxl.packagebrowser.compile.LibraryApi", {
 
     async load() {
       let command = this.getCompilerApi().getCommand();
-      command.addListener("writtenApplication", (e) => this.__appCompiling(e.getData()));
+      command.addListener("writtenApplication", this.__onWrittenApplication, this);
     },
 
-    __appCompiling(application) {
+    __onWrittenApplication(e) {
+      let application = e.getData();
       let className = application.getClassName();
       if (className !== "qxl.packagebrowser.Application") {
         return;
       }
-      if (this.__pkgDataGenerated) {
-        return;
-      }
-	  
-      console.info(`\n`);
       let maker = this.getCompilerApi().getCommand().getMaker();
       const outputDir = maker.getTarget().getOutputDir();
       let app = application.getName();
-      const path = require("path");
-      const {execSync} = require('child_process');
-	  let datafile = path.join(outputDir, app, "package-data.json");
-      let cmds = [
-        `qx pkg update`,
-        `qx pkg list --json --all > ${datafile}`
-      ];
+      let datafile = path.join(outputDir, app, "package-data.json");
+      let cmds = [];
+      if (!this.__pkgDataGenerated) {
+        cmds.push(`qx pkg update`);
+        cmds.push(`qx pkg list --json --all > ${datafile}`);
+        this.__execute(cmds);
+        this.__pkgDataGenerated = true;
+      }
+    },
+
+    __execute(cmds) {
       for (let cmd of cmds){
         console.info(`>>> Executing '${cmd}'`);
         execSync(cmd, { stdio: 'inherit' });
       }
-      this.__pkgDataGenerated = true;
     }
   }
 });
